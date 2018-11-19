75148c3a9762e3572006410632f9606fe8dc95eb
Shadrack Taeli <shadrack@cobiinteractive.com>
2018-11-19 22:12:17 +0200

    Initial commit

---
+++ networkimageview/src/main/java/org/shadracktaeli/networkimageview/NetworkImageView.kt
@@ -0,0 +1,9 @@
+package org.shadracktaeli.networkimageview
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatImageView
+
+class NetworkImageView @JvmOverloads constructor(context: Context, attributeSet: AttributeSet? = null): AppCompatImageView(context, attributeSet) {
+
+}
